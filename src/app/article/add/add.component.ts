de3a0e2fa19027aa5ddb1701aace94b53dab49d8
Maciej Kulesza <maciej.kulesza@blueservices.pl>
2019-08-01 10:36:16 +0200

    almost ready TODO: add authentication token in json; add routing to comp

--- src/app/article/add/add.component.ts
+++ src/app/article/add/add.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
-
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ApiService} from '../../core/services';
 @Component({
-  selector: 'app-add',
+  selector: 'app-add-article-form',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.sass']
 })
 export class AddComponent implements OnInit {
-  articleForm: FormGroup;
+  addArticleForm: FormGroup;
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private apiService: ApiService
   ) {
-    this.articleForm = this.fb.group({
-      'header': [''],
-      'contentPol': [''],
-      'image': ['']
-    })
+    this.addArticleForm = this.fb.group({
+      title: ['', Validators.required],
+      body: ['', Validators.required],
+      image: ['']
+    });
   }
 
   ngOnInit() {
   }
 
+  submitForm() {
+    const article = this.addArticleForm.value;
+    console.log(this.apiService.post('articles/', {article}).subscribe());
+  }
 }
