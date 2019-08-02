1991503b554bdc4fe34c3e2f71c9a824cfea0a9c
Maciej Kulesza <maciej.kulesza@blueservices.pl>
2019-08-02 12:54:28 +0200

    little changes, still working on it

--- src/app/article/add/add.component.ts
+++ src/app/article/add/add.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ApiService, JwtService} from '../../core/services';
 import {HttpHeaders} from '@angular/common/http';
-
+import {Article} from '../article'
 @Component({
   selector: 'app-add-article-form',
   templateUrl: './add.component.html',
@@ -10,6 +10,8 @@ import {HttpHeaders} from '@angular/common/http';
 })
 export class AddComponent implements OnInit {
   addArticleForm: FormGroup;
+  article: Article = new Article();
+
   constructor(
     private fb: FormBuilder,
     private apiService: ApiService,
@@ -18,15 +20,21 @@ export class AddComponent implements OnInit {
     this.addArticleForm = this.fb.group({
       title: ['', Validators.required],
       body: ['', Validators.required],
-      image: ['']
+      image: ['', Validators.required]
     });
   }
 
   ngOnInit() {
   }
-
+  handleUpload(e) {
+    const file = e.target.files[0];
+    const fileReader = new FileReader();
+    console.log(this.article);
+    this.article.image = file;
+    console.log(this.article.image);
+  }
   submitForm() {
-    const article = this.addArticleForm.value;
-    this.apiService.post('articles/', article).subscribe();
+    this.article = this.addArticleForm.value;
+    this.apiService.post('articles/', this.article).subscribe();
   }
 }
---
+++ src/app/article/article.ts
@@ -0,0 +1,5 @@
+export class Article {
+  title: String;
+  body: String;
+  image: File;
+}
